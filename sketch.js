8b710103b039b1b5803c28830c9a984c04bc657b
Frederick Ostrenko <fredeerock@a4-5e-60-e9-b2-29.wlan.lsu.edu>
2019-12-05 16:40:36 -0600

    wait

--- sketch.js
+++ sketch.js
@@ -3,29 +3,24 @@ var raw = new Image();
 
 // Make sure ip and port match in Runway
 var socket = io.connect('http://127.0.0.1:3000');
-
-// Load all the DOM elements first
-document.addEventListener("DOMContentLoaded", function(event) {
   
-  // Let us know when a connection is established
-  socket.on('connect', function() {
-    console.log("connected")
-  });
-  
-  // When data is sent update the raw source. Sometimes this is data.output instead.
-  socket.on('data', function(data) {
-      console.log(data);
-      raw.src = data.result;
-      // raw.src = data.output;
-  });
-
-  // Wait for the image data to load then draw it to canvas.
-  raw.onload = function() {
-    img = createImage(raw.width, raw.height);
-    img.drawingContext.drawImage(raw, 0, 0);
-  }
-
+// Let us know when a connection is established
+socket.on('connect', function() {
+  console.log("connected")
 });
+
+// When data is sent update the raw source. Sometimes this is data.output instead.
+socket.on('data', function(data) {
+    console.log(data);
+    raw.src = data.result;
+    // raw.src = data.output;
+});
+
+// Wait for the image data to load then draw it to canvas.
+raw.onload = function() {
+  img = createImage(raw.width, raw.height);
+  img.drawingContext.drawImage(raw, 0, 0);
+}
 
 // Begin normal p5 stuff.
 function setup() {
@@ -34,9 +29,10 @@ function setup() {
 }
 
 function draw() {
+
+  // Don't try to draw the image before it's loaded.
   if(typeof img !== "undefined") {
     image(img, 0, 0);
   }
 
-  print(get(mouseX, mouseY));
 }
